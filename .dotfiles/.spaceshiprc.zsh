9204198a383a52cdfb7931561c9d40711d2552e9
Ryan Field <fieldryan19@gmail.com>
2025-11-04 23:19:21 +1300

    Configures Spaceship prompt
    Configures the Spaceship prompt with desired settings, including displaying time and username, disabling path truncation in repos, enabling async prompt, and defining prompt order for optimal performance.
    
    The changes aim to customize the shell prompt for a more informative and efficient user experience.

--- .dotfiles/.spaceshiprc.zsh
+++ .dotfiles/.spaceshiprc.zsh
@@ -6,3 +6,17 @@ SPACESHIP_USER_SHOW=always
 
 # Do not truncate path in repos
 SPACESHIP_DIR_TRUNC_REPO=false
+
+SPACESHIP_PROMPT_ASYNC=true
+SPACESHIP_PROMPT_ADD_NEWLINE=true
+SPACESHIP_CHAR_SYMBOL="⚡"
+
+# Minimal spaceship sections for performance
+SPACESHIP_PROMPT_ORDER=(
+  time
+  user
+  dir
+  git
+  line_sep
+  char
+)
